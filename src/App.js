ed205c54c01788be5192ffce122527d761220082
yyx0111 <749857410@qq.com>
2022-06-05 14:23:04 +0800

    修改

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import React from "react";
 import './App.css';
-import {BrowserRouter,Routes ,Link} from 'react-router-dom'
+import {BrowserRouter, Routes, Link} from 'react-router-dom'
 
 function App() {
    return (
